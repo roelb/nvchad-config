c7c550242a46de5cc47226e5c341795ca4a56497
R. <393580+roelb@users.noreply.github.com>
2024-08-18 14:17:04 +0200

    added ts requirements, added vim-surround

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -41,14 +41,37 @@ return {
       { ",v", "<cmd>VenvSelect<cr>" },
     },
   },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim",
+        "lua",
+        "vimdoc",
+        "html",
+        "css",
 
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+        "bash",
+        "python",
+        "dockerfile",
+
+        "c",
+        "cpp",
+        "make",
+
+        "yaml",
+        "json",
+      },
+    },
+  },
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup {
+        -- Configuration here, or leave empty to use defaults
+      }
+    end,
+  },
 }
